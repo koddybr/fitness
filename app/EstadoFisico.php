ce4c1d8edc116e20f2be491bba2f41ec84898004
Vrian7 <brian.barrera.bo@gmail.com>
2019-10-22 14:52:46 -0400

    modelos

---
+++ app/EstadoFisico.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EstadoFisico extends Model
+{
+    protected $table = 'estados_fisicos';
+}
---
+++ app/Http/Controllers/EstadoFisicoController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EstadoFisicoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $estados_fisicos = EstadoFisico::get();
+        $data = [
+            'estados_fisicos' => $estados_fisicos
+        ];
+
+        return view('estados_fisicos.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('estados_fisicos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $estado_fisico = new EstadoFisico();
+        $estado_fisico->nombre = $request->nombre;
+        $estado_fisico->descripcion = $request->descripcion;
+        $estado_fisico->factor = $request->factor;
+        $estado_fisico->save();
+        $data = [
+            'estado_fisico' => $estado_fisico
+        ];
+        return redirect('estados_fisicos');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
