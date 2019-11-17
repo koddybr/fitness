ad9aa7620536718c48328e2cb144f97750e74e7b
koddybr <koddybr@gmail.com>
2019-11-17 12:47:42 -0400

    apis

--- app/Http/Controllers/FitlabelAndroidController.php
+++ app/Http/Controllers/FitlabelAndroidController.php
@@ -12,6 +12,48 @@ class FitlabelAndroidController extends Controller
         $data = [
             'alimentos' => $alimentos
         ];
-        return response()->json($data);
+        return response()->json($alimentos);
+    }
+
+    public function getMacronutrientes(){
+        $macronutrientes = \App\Macronutriente::get();
+
+        return response()->json($macronutrientes);
+    }
+
+    public function getEstadosFisicos(){
+        $estados_fisicos = \App\EstadoFisico::get();
+
+        return response()->json($estados_fisicos);
+    }
+
+    public function getNivelesActividades() {
+        $niveles = \App\Actividad::get();
+
+        return response()->json($niveles);
+    }
+
+    public function getRecomendaciones() {
+        $recomendaciones = \App\Recomendacion::get();
+
+        return response()->json($recomendaciones);
+    }
+
+    public function getLogros() {
+        $logros = \App\Logro::get();
+
+        return response()->json($logros);
+    }
+
+    public function getAvances() {
+        $avances = \App\Avance::get();
+
+        return response()->json($avances);
+    }
+
+    public function getUsuarios(){
+        $users = \App\User::get();
+
+        return  response()->json($users);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,15 @@ Route::resource('recomendaciones','RecomendacionController');
 Route::resource('logros','LogroController');
 Route::resource('avances','AvanceController');
 
+/**
+ * API para android
+ */
 //Route::post('user','UserController');
 Route::get('get_alimentos','FitlabelAndroidController@getAlimentos');
+Route::get('get_macronutrientes','FitlabelAndroidController@getMacronutrientes');
+Route::get('get_estados_fisicos','FitlabelAndroidController@getEstadosFisicos');
+Route::get('get_niveles_actividades','FitlabelAndroidController@getNivelesActividades');
+Route::get('get_recomendaciones','FitlabelAndroidController@getRecomendaciones');
+Route::get('get_logros','FitlabelAndroidController@getLogros');
+Route::get('get_avances','FitlabelAndroidController@getAvances');
+Route::get('get_usuarios','FitlabelAndroidController@getUsuarios');
